1b0663a0386e5644a66576ff56c83736e18b67b3
Martin Forsgren <martin.forsgren@gmail.com>
2013-09-27 18:11:25 +0200

    Add clojurescript, core.async and dommy.

--- project.clj
+++ project.clj
@@ -3,4 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1889"]
+                 [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
+                 [prismatic/dommy "0.1.1"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
+  :cljsbuild
+  {:builds
+   [{:id "simple"
+     :source-paths ["src/cljs_test"]
+     :compiler {:optimizations :simple ;:advanced
+                :pretty-print false
+                :output-dir "out"
+                :output-to "main.js"
+                :source-map "main.js.map"}}]})
