889d76c87f30e8bea5cbdaaf5b1d88c6038ff87a
Martin Forsgren <martin.forsgren@gmail.com>
2013-10-19 23:55:18 +0200

    update core.async

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1889"]
-                 [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
                  [prismatic/dommy "0.1.1"]]
   :plugins [[lein-cljsbuild "0.3.3"]]
   :cljsbuild
