70e48507d78e8878975f0eefb2c06d4bfde159ce
Martin Forsgren <martin.forsgren@gmail.com>
2013-11-30 23:21:16 +0100

    Add server deps: http-kit, ring, compojure. Update cljs, dommy.

--- project.clj
+++ project.clj
@@ -4,9 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1889"]
+                 [org.clojure/clojurescript "0.0-2030"]
                  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
-                 [prismatic/dommy "0.1.1"]]
+                 [prismatic/dommy "0.1.2"]
+                 [http-kit "2.1.13"]
+                 [ring "1.2.1"]
+                 [compojure "1.1.5"]]
   :plugins [[lein-cljsbuild "0.3.3"]]
   :cljsbuild
   {:builds
