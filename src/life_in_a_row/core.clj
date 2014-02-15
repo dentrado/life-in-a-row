3443249bd55bfee525190b201c91eea68951a397
Martin Forsgren <martin.forsgren@gmail.com>
2014-02-15 21:24:09 +0100

    Rethinked server, new-game returns p1id and p2id instead of gameid. WIP
    Replace client atom with opponent and channels atoms.

--- src/life_in_a_row/core.clj
+++ src/life_in_a_row/core.clj
@@ -8,29 +8,73 @@
             [ring.middleware.resource :refer [wrap-resource]]
             [ring.middleware.file-info :refer [wrap-file-info]]))
 
-(def clients (atom {})) ;player(uuid) -> channel
+;; State
+(def opponents (atom {})) ; player-id -> player-id
+(def channels (atom {}))  ; player-id -> channel
 
+(defn get-opponent-chan [user-id]
+  (@channels (@opponents user-id)))
+
+;; Logic
 (defn new-uuid []
   (java.util.UUID/randomUUID))
 
-(defn move-handler [req]
-  (hk/with-channel req chan
-    (swap! clients assoc :en-uuid chan)
-    (println chan " connected.")
-    (hk/on-close chan (fn [status]
-                        (swap! clients dissoc :en-uuid)
-                        (println chan " disconnected. status: " status)))
-    (println (:body req))))
+(defn new-game-handler [_]
+  (let [p1 (new-uuid)
+        p2 (new-uuid)]
+    (swap! opponents assoc p1 p2 p2 p1)
+    (pr-str [p1 p2])))
 
+(defn join-handler
+  [player-id request]
+  (hk/with-channel request chan
+                   (swap! channels assoc player-id chan) ; TODO add check if right num of players.
+                   (println chan " connected.")
+                   (hk/on-close chan (fn [status]
+                                       (swap! channels dissoc player-id)
+                                       (println chan " disconnected. status: " status))))
+  (println (:session request)))
+
+(defn move-handler [{:keys [player-id body]}]
+  (if-let [chan (get-opponent-chan player-id)]
+    (hk/send! chan body)
+    (println "ERROR: opponent not there!")))
+
+;; Routing
 (defroutes life
+
   (GET "/" [] (resource-response "index.html"))
-  (POST "/new-game" [] (str (new-uuid)))
-  (POST "/move" [] move-handler))
+
+  (POST "/new-game" [] new-game-handler) ;; TODO: cleanup after a while if noone connects
+
+  (POST "/join-game/:player-id" {{player-id :player-id} :params :as request}
+        (join-handler player-id request))
+
+  (POST "/move" [] move-handler)) ;render on a fn calls the fn
+
+
+
+(def app
+  (-> #'life
+      handler/site ; includes wrap-session
+      (wrap-resource ".")
+      wrap-file-info))
 
 (defn -main [& args]
-  (hk/run-server
-   (-> #'life
-       handler/site
-       (wrap-resource ".")
-       wrap-file-info)
-   {:port 4001}))
+  (hk/run-server #'app {:port 4001}))
+
+;(-main)
+
+;; Unused
+
+(defn wrap-user-id [handler]
+  (fn [req]
+    (let [user-id (or (get-in req [:session :user-id]) (new-uuid))]
+      (-> req
+          (assoc-in [:session :user-id] user-id)
+          (assoc :user-id user-id)
+          handler))))
+
+@channels
+@opponents
+(reset! opponents)
